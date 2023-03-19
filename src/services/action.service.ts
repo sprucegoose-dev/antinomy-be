ae5ded1c724344ae377b68044ce6a300c9edb5cf
Morgan Polak <polakmaor@gmail.com>
2023-03-19 22:04:45 +0000

    ENH: adds the Deploy and Replace actions to the ActionService class

--- src/services/action.service.ts
+++ src/services/action.service.ts
@@ -1,27 +1,75 @@
 import { Card } from '../models/card.model';
 import { Player } from '../models/player.model';
 import { ActionType, IActionPayload } from '../types/action.interface';
+import { Color } from '../types/card_type.interface';
 import { PlayerOrientation } from '../types/player.interface';
 
 export class ActionService {
 
+    static getDeployAction(codexColor: Color, continuumCards: Card[]): IActionPayload[] {
+        let actions: IActionPayload[] = [];
+
+        for (const card of continuumCards) {
+            if (card.type.color === codexColor) {
+                actions.push({
+                    targetIndex: card.index,
+                    type: ActionType.DEPLOY,
+                });
+            }
+        }
+
+        return actions;
+    }
+
     static getMoveActions(player: Player, cardsInHand: Card[], continuumCards: Card[]): IActionPayload[] {
+        let actions: IActionPayload[] = [];
+        const {
+            cardsInPast,
+            cardsInFuture,
+        } = this.getPastAndFutureCards(player, continuumCards);
+
+        for (const card of cardsInHand) {
+            actions = actions.concat(this.getValidMoveTargets(card, cardsInPast, cardsInFuture));
+        }
+
+        return actions;
+    }
+
+    static getPastAndFutureCards(player: Player, continuumCards: Card[]): { cardsInPast: Card[], cardsInFuture: Card[] } {
         const {
             position: playerPosition,
             orientation,
         } = player;
 
-        let actions: IActionPayload[] = [];
-
         if (orientation === PlayerOrientation.TOP) {
             continuumCards.reverse();
         }
 
-        const cardsInPast = continuumCards.slice(0, playerPosition);
-        const cardsInFuture = continuumCards.slice(playerPosition);
+        return {
+            cardsInPast: continuumCards.slice(0, playerPosition),
+            cardsInFuture: continuumCards.slice(playerPosition),
+        };
+    }
 
-        for (const card of cardsInHand) {
-            actions = actions.concat(this.getValidMoveTargets(card, cardsInPast, cardsInFuture));
+    static getReplaceAction(player: Player, continuumCards: Card[]): IActionPayload[] {
+        let actions: IActionPayload[] = [];
+        const {
+            cardsInPast,
+            cardsInFuture,
+        } = this.getPastAndFutureCards(player, continuumCards);
+
+        if (cardsInPast.length >= 3) {
+            actions.push({
+                targetIndex: cardsInPast[cardsInPast.length - 1].index,
+                type: ActionType.REPLACE,
+            });
+        }
+
+        if (cardsInFuture.length >= 3) {
+            actions.push({
+                targetIndex: cardsInFuture[0].index,
+                type: ActionType.REPLACE,
+            });
         }
 
         return actions;
@@ -36,7 +84,7 @@ export class ActionService {
                     sourceCardId: cardInHand.id,
                     targetIndex: card.index,
                     type: ActionType.MOVE,
-                })
+                });
             }
         }
 
@@ -47,7 +95,7 @@ export class ActionService {
                 sourceCardId: cardInHand.id,
                 targetIndex: cardInFuture.index,
                 type: ActionType.MOVE,
-            })
+            });
         }
 
         return actions;
